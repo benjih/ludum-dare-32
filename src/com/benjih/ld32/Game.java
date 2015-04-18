b18b9b684dd5f1cb1c3b7fc9e6c1f94579980f7b
Benji Hooper <benji@sixp.in>
2015-04-18 08:56:27 +0100

    Press a key and a card disappears

--- src/com/benjih/ld32/Game.java
+++ src/com/benjih/ld32/Game.java
@@ -2,6 +2,7 @@ package com.benjih.ld32;
 
 import java.util.Map;
 
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.opengl.Texture;
 
 public class Game {
@@ -10,20 +11,34 @@ public class Game {
 	private Sprite card2;
 	private Sprite card3;
 	private Sprite card4;
+	
+	private int playerHealth;
+	private int enemyHealth;
 
 	public Game (Map<String, Texture> textureMap) {
 		card1 = new PlayingCard(PlayingCardPosition.POS_1, textureMap.get("basic-card"));
 		card2 = new PlayingCard(PlayingCardPosition.POS_2, textureMap.get("basic-card"));
 		card3 = new PlayingCard(PlayingCardPosition.POS_3, textureMap.get("basic-card"));
  		card4 = new PlayingCard(PlayingCardPosition.POS_4, textureMap.get("basic-card"));
+ 		playerHealth = 30;
+ 		enemyHealth = 30;
 	}
 
 	public void run () {
+		render();
 		
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			System.out.println("hello");
+			card1.hide();
+		}
+
+	}
+
+	private void render() {
 		card1.render();
 		card2.render();
 		card3.render();
-		card4.render();
+		card4.render();		
 	}
 
 }
