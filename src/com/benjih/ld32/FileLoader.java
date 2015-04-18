1f324d054c2d04d32f5ca8b9359b2ddbd96cbd90
Benji Hooper <benji@sixp.in>
2015-04-18 14:41:54 +0100

    Added TrueTypeFont loading

--- src/com/benjih/ld32/FileLoader.java
+++ src/com/benjih/ld32/FileLoader.java
@@ -1,6 +1,8 @@
 package com.benjih.ld32;
 
+import java.awt.Font;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
@@ -12,6 +14,18 @@ public class FileLoader {
 		try {
 			return TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileUri));
 		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+	}
+	
+	public static Font loadFont(String fileUri) {
+		try {
+			InputStream inputStream = ResourceLoader.getResourceAsStream(fileUri);
+
+			return Font.createFont(Font.TRUETYPE_FONT, inputStream);
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
