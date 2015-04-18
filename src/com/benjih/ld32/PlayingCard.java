1bde88e882ec16fdde25a27e57a539a60b732ca1
Benji Hooper <benji@sixp.in>
2015-04-18 11:24:40 +0100

    PlayingCards now have stats

--- src/com/benjih/ld32/PlayingCard.java
+++ src/com/benjih/ld32/PlayingCard.java
@@ -4,16 +4,26 @@ import org.newdawn.slick.opengl.Texture;
 
 public class PlayingCard extends Sprite {
 	
-	private PlayingCardPosition position;
-
-	public PlayingCard(Texture resource) {
+	private int damage;
+	private int armour;
+	
+	public PlayingCard (Texture resource, int damage, int armour) {
 		super(0, 0, resource);
+		this.damage = damage;
+		this.armour = armour;
 	}
 	
-	public void setPosition(PlayingCardPosition position) {
-		this.position = position;
+	public void setPosition (PlayingCardPosition position) {
 		setX(position.getX());
 		setY(position.getY());
+	}
+	
+	public int getDamage () {
+		return damage;
+	}
+	
+	public int getArmour () {
+		return armour;
 	}
 
 }
